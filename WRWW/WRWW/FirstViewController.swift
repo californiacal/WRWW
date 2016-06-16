964122dfd1bcabffb715ede221231492499cbf8d
John P. Swensen <jpswensen@swengames.com>
2016-06-15 22:02:04 -0700

    Created the CocoaPods workspace and added one pod. Ready to start coding.

---
+++ WRWW/WRWW/FirstViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  WRWW
+//
+//  Created by John Swensen on 6/15/16.
+//  Copyright © 2016 WRWW. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
